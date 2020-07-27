6b1d0c4a2fc4ecc3bc8733cdcd2c5d8b975ea0d3
asmames <66780515+asmames@users.noreply.github.com>
2020-07-27 11:52:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+import picture from './imageInSrc.jpg';
+import video from './myVideo.mp4';
+function App() {
+  return (
+    <div className="App">
+      <div style={{
+            border:'solid 1px black' ,
+            maxWidth : "100vw"}}>  
+
+              <h1 className ="title Red">Your name here</h1>  
+
+              <img src="imageInPublic.jpg" />
+
+              <img src = {picture} />
+
+
+          </div>  
+
+          <video width="320" height = "240" controls>
+
+              <source src={video} type ='video/mp4'></source>
+
+          </video>
+      
+    </div>
+  );
+}
+
+export default App;
